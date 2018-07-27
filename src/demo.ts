d695a2c609c904926b6612a95b3f60162dbf64b5
FlowerEatFish <flower.eating.fish@gmail.com>
2018-07-27 14:28:05 +0800

    Add an example for demo

--- src/demo.ts
+++ src/demo.ts
@@ -6,8 +6,9 @@
 import bookCollection from './index';
 
 const keywordList: string[] = [
-  '相對論，愛因斯坦教會我們什麼',
-  'blablablablablablablablablablablabla'
+  '春夏秋冬，日日食光。生活美學家的鑄鐵鍋料理手帖', // It will get one result as expected.
+  '相對論，愛因斯坦教會我們什麼', // It will get more results on 12 ones per page as expected.
+  'blablablablablablablablablablablabla' // It will be no result.
 ];
 
 const demo: Function = async (): Promise<void> => {
